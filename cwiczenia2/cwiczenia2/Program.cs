49ccf6e1248944ffa47940984b187c0d2c8a6de7
s27468 <zuzanna.lidia@gmail.com>
2024-03-08 15:17:49 +0100

    Average method

--- cwiczenia2/cwiczenia2/Program.cs
+++ cwiczenia2/cwiczenia2/Program.cs
@@ -4,3 +4,14 @@ Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+
+static double GetAverage(int [] numbers )
+{
+    double result = 0.0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        result += numbers[i];
+    }
+
+    return result / numbers.Length;
+}
